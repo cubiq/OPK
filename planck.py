5ff5a61b22bfa988852e84725c1ec59189838eee
alin m elena <alinm.elena@gmail.com>
2022-09-01 16:34:21 +0100

    more fixes

--- planck.py
+++ planck.py
@@ -37,7 +37,7 @@ for row,ll,ff in zip(leg,lay,fonts):
             convex=True
         scoop = 2.5
         if k in ['f','F','j','J']:
-            scoop = 3.5
+            scoop = 2.5*1.05
         planck.add(keycap(legend=k,
                        angle=angles[i],
                        font=f,
