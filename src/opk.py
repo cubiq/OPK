d12312aafe4a03eb9f977e50ca78dd2e08c03ca3
Alin Marin Elena <alin@elena.space>
2022-09-24 07:23:15 +0100

    update

--- src/opk.py
+++ src/opk.py
@@ -1,12 +1,12 @@
 """
 ==========================
-  ██████  ██████  ██   ██ 
- ██    ██ ██   ██ ██  ██  
- ██    ██ ██████  █████   
- ██    ██ ██      ██  ██  
-  ██████  ██      ██   ██ 
+  ██████  ██████  ██   ██
+ ██    ██ ██   ██ ██  ██
+ ██    ██ ██████  █████ 
+ ██    ██ ██      ██  ██ 
+  ██████  ██      ██   ██
 ==========================
- Open Programmatic Keycap 
+ Open Programmatic Keycap
 ==========================
 
 OPK is a spherical top keycap profile developed in CadQuery
@@ -23,6 +23,7 @@ Copyright (c) 2022 Matteo "Matt3o" Spinelli
 https://matt3o.com
 """
 
+import math
 import cadquery as cq
 
 def keycap(
@@ -40,8 +41,9 @@ def keycap(
     convex: bool = False,       # Is this a spacebar?
     legend: str = "",           # Legend
     legendDepth: float = -1.0,  # How deep to carve the legend, positive value makes the legend embossed
-    font: str = "sans-serif",
-    fontsize: float = 10
+    font: str = "sans-serif",   # font name, use a font name including extension to use a local file
+    fontsize: float = 10,       # the font size is in units
+    pos: bool = False           # use POS style stabilizers
 ):
 
     top_diff = base - top
@@ -55,7 +57,10 @@ def keycap(
     ty = by - top_diff
 
     # if spacebar make the top less round-y
-    tension = .4 if convex else 1 
+    tension = .4 if convex else 1
+
+    if unitX < 2 and unitY < 2:
+        pos = False
 
     # Three-section loft of rounded rectangles. Can't find a better way to do variable fillet
     base = (
@@ -96,7 +101,7 @@ def keycap(
 
     # Create a body that will be carved from the main shape to create the top scoop
     if convex:
-        tool = (
+        scoop = (
             cq.Workplane("YZ").transformed(offset=cq.Vector(0, height-2.1, -bx/2), rotate=cq.Vector(0, 0, angle))
             .moveTo(-by/2, -1)
             .threePointArc((0, 2), (by/2, -1))
@@ -106,7 +111,7 @@ def keycap(
             .extrude(bx, combine=False)
         )
     else:
-        tool = (
+        scoop = (
             cq.Workplane("YZ").transformed(offset=cq.Vector(0, height, bx/2), rotate=cq.Vector(0, 0, angle))
             .moveTo(-by/2+2,0)
             .threePointArc((0, min(-0.1, -depth+1.5)), (by/2-2, 0))
@@ -129,7 +134,7 @@ def keycap(
         )
 
     #show_object(tool, options={'alpha': 0.4})
-    keycap = keycap - tool
+    keycap = keycap - scoop
     
     # Top edge fillet
     keycap = keycap.edges(">Z").fillet(0.6)
@@ -139,44 +144,60 @@ def keycap(
     shell = (
         cq.Workplane("XY").rect(bx-thickness*2, by-thickness*2)
         .workplane(offset=height/4).rect(bx-thickness*3, by-thickness*3)
-        .workplane().transformed(offset=cq.Vector(0, 0, height-height/4-4.5), rotate=cq.Vector(angle, 0, 0)).rect(tx-thickness*2, ty-thickness*2)
+        .workplane().transformed(offset=cq.Vector(0, 0, height-height/4-4.5), rotate=cq.Vector(angle, 0, 0)).rect(tx-thickness*2+.5, ty-thickness*2+.5)
         .loft()
     )
     keycap = keycap - shell
 
-    # Build the stem and the keycap guts
-    # TODO: we need to find a better way to build the stem...
-    if ( unitY > unitX ):
-        if unitY > 1.75:
-            dist = 2.25 / 2 * 19.05 - 19.05 / 2
-            stem_pts = [(0, 0), (0, dist), (0, -dist)]
-            stem1 = (
-                cq.Sketch()
-                .rect(0.8, ty)
-                .push(stem_pts)
-                .rect(tx, 0.8)
-                .circle(2.75)
-                .clean()
-            )
-    else:
-        if unitX < 2:
-            stem_pts = [(0,0)]
-        elif unitX < 3:     # keycaps smaller than 3unit all have 2.25 stabilizers
-            dist = 2.25 / 2 * 19.05 - 19.05 / 2
-            stem_pts = [(0,0), (dist, 0), (-dist,0)]
-        else:
-            dist = unitX / 2 * 19.05 - 19.05 / 2
-            stem_pts = [(0,0), (dist, 0), (-dist,0)]
+    # create a temporary surface that will be used to project the stems to
+    # this is needed because extrude(face) needs the entire extruded outline to be contained inside the destination face
+    tmpface = shell.faces('>Z').workplane().rect(bx*2, by*2).val()
+    tmpface = cq.Face.makeFromWires(tmpface)
 
-        stem1 = (
-            cq.Sketch()
-            .rect(tx, 0.8)
-            .push(stem_pts)
-            .rect(0.8, ty)
-            .circle(2.75)
-            .clean()
-        )
-    
+    # Build the stem and the keycap guts
+
+    if pos:     # POS-like stems
+        stem_pts = []
+        ribh_pts = []
+        ribv_pts = []
+
+        stem_num_x = math.floor(unitX)
+        stem_num_y = math.floor(unitY)
+        stem_start_x = round(-19.05 * (stem_num_x / 2) + 19.05 / 2, 6)
+        stem_start_y = round(-19.05 * (stem_num_y / 2) + 19.05 / 2, 6)
+
+        for i in range(0, stem_num_y):
+            ribh_pts.extend([(0, stem_start_y+i*19.05)])
+            for l in range(0, stem_num_x):
+                if i == 0:
+                    ribv_pts.extend([(stem_start_x+l*19.05, 0)])
+                stem_pts.extend([(stem_start_x+l*19.05, stem_start_y+i*19.05)])
+
+    else:       # standard stems
+        stem_pts = [(0,0)]
+
+        if ( unitY > unitX ):
+            if unitY > 2.75:
+                dist = unitY / 2 * 19.05 - 19.05 / 2
+                stem_pts.extend([(0, dist), (0, -dist)])
+            elif unitY > 1.75:
+                dist = 2.25 / 2 * 19.05 - 19.05 / 2
+                stem_pts.extend([(0, -dist), (0, dist)])
+            
+            ribh_pts = stem_pts
+            ribv_pts = [(0,0)]
+        else:
+            if unitX > 2.75:
+                dist = unitX / 2 * 19.05 - 19.05 / 2
+                stem_pts.extend([(dist, 0), (-dist,0)])
+            elif unitX > 1.75:      # keycaps smaller than 3unit all have 2.25 stabilizers
+                dist = 2.25 / 2 * 19.05 - 19.05 / 2
+                stem_pts.extend([(dist, 0), (-dist,0)])
+            
+            ribh_pts = [(0,0)]
+            ribv_pts = stem_pts
+
+    # this is the stem +
     stem2 = (
         cq.Sketch()
         .push(stem_pts)
@@ -186,9 +207,16 @@ def keycap(
     )
 
     keycap = (
-        keycap.faces("<Z").transformed(offset=cq.Vector(0, 0, 4.5))
-        .placeSketch(stem1)
-        .extrude("next")
+        keycap.faces("<Z").transformed(offset=cq.Vector(0, 0, 4.5)).workplane()
+        .pushPoints(stem_pts)
+        .circle(2.75)
+        .extrude(tmpface)
+        .pushPoints(ribh_pts)
+        .rect(tx, 0.8)
+        .extrude(tmpface)
+        .pushPoints(ribv_pts)
+        .rect(0.8, ty)
+        .extrude(tmpface)
         .faces("<Z").workplane(offset=-0.6)
         .pushPoints(stem_pts)
         .circle(2.75)
@@ -207,38 +235,38 @@ def keycap(
             fontPath = font
             font = ''
 
-        legend = (
-            cq.Workplane("XY").transformed(offset=cq.Vector(0, 0, height+1), rotate=cq.Vector(angle, 0, 0))
-            .text(legend, fontsize, -4, font=font, fontPath=fontPath, halign="center", valign="center")
-        )
-        bb = legend.val().BoundingBox()
-        # try to center the legend horizontally
-        legend = legend.translate((-bb.center.x, 0, 0))
-
+        if legend.endswith('.dxf'):
+            legend = (
+                cq.importers
+                .importDXF(legend)
+                .wires().toPending()
+                .extrude(-4)
+                .translate((0,0,height+1))
+                .rotateAboutCenter((1,0,0), angle)
+            )
+            # center the legend
+            bb = legend.val().BoundingBox()
+            legend = legend.translate((-bb.center.x, -bb.center.y, 0))
+        else:
+            legend = (
+                cq.Workplane("XY").transformed(offset=cq.Vector(0, 0, height+1), rotate=cq.Vector(angle, 0, 0))
+                .text(legend, fontsize, -4, font=font, fontPath=fontPath, halign="center", valign="center")
+            )
+            bb = legend.val().BoundingBox()
+            # only center horizontally to keep the baseline
+            legend = legend.translate((-bb.center.x, 0, 0))
+        
         if legendDepth < 0:
             legend = legend - keycap
             legend = legend.translate((0,0,legendDepth))
             keycap = keycap - legend
-            legend = legend - tool      # this can be used to export the legend for 2 colors 3D printing
+            legend = legend - scoop      # this can be used to export the legend for 2 colors 3D printing
         else:
-            tool = tool.translate((0,0,legendDepth))
-            legend = legend - tool
+            scoop = scoop.translate((0,0,legendDepth))
+            legend = legend - scoop
             legend = legend - keycap    # use this for multi-color 3D printing
             keycap = keycap + legend
 
         #show_object(legend, name="legend", options={'color': 'blue', 'alpha': 0})
-
-    """
-    # Example to cut a DXF logo out of the keycap
-    logo = (
-        cq.importers
-        .importDXF('logo.dxf')
-        .wires().toPending()
-        .extrude(-2)
-        .translate((-5,-4.6,height)) # needs centering
-        .rotateAboucurventer((1,0,0), angle)
-    )
-    keycap = keycap - logo
-    """
 
     return keycap
